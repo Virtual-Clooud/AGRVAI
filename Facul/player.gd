69827d8b4a7d89ef743aba03b8418c080a14dda9
Tiago Carvalho <warkdem@gmail.com>
2022-09-27 10:34:38 -0300

    0.0 Faça-se a luz

---
+++ Facul/player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var life = 10
+var speed = 300
+var jumpF = 500
+var fused = true
+var red = 0
+var blue = 0
+var green = 0
+
+func red():
+	pass
+func blue():
+	pass
+func green():
+	pass
+func _ready():
+	pass
+
+func ded():
+	pass
+ 
+func _physics_process(delta):
+	pass
